a9de16bbffae0a4189525357b0582d526c2b137c
zyxucp <286513187@qq.com>
2025-04-01 15:03:07 +0800

    update tool

---
+++ src/MCPP.Net/Tools/TestTool.cs
@@ -0,0 +1,15 @@
+﻿using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+namespace MCPP.Net.Tools
+{
+    [McpServerToolType]
+    public static class TestTool
+    {
+        [McpServerTool(Name = "test"), Description("这是一个测试函数，用于测试MCP Tool")]
+        public static string Test([Description("测试消息")] string message)
+        {
+            return "hello " + message;
+        }
+    }
+}
