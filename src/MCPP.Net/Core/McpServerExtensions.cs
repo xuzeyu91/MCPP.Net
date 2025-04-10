e02b1b2ecfa2875218aa549c1cbe94235e303d94
ihourglass <inversionhourglass@outlook.com>
2025-04-11 02:55:55 +0800

    添加 McpToolsKeeper 管理 所有 McpServerTool，方便针对程序集进行批量操作

---
+++ src/MCPP.Net/Core/McpServerExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Options;
+using ModelContextProtocol.Server;
+
+namespace MCPP.Net.Core
+{
+    /// <summary>
+    /// <see cref="IMcpServerBuilder"/> extension methods
+    /// </summary>
+    public static class McpServerExtensions
+    {
+        /// <summary>
+        /// 使用 <see cref="McpToolsKeeper"/> 管理 Tools，方便后续的增删操作
+        /// </summary>
+        public static IMcpServerBuilder UseToolsKeeper(this IMcpServerBuilder builder)
+        {
+            builder.Services.AddSingleton<McpToolsKeeper>();
+            builder.Services.AddTransient<IPostConfigureOptions<McpServerOptions>, McpServerOptionsPostConfigure>();
+
+            return builder;
+        }
+    }
+}
