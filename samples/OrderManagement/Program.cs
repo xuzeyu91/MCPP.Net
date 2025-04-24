84bdebf1a57c1905aed8f6b445b3bc4caaba5028
ihourglass <inversionhourglass@outlook.com>
2025-04-24 17:49:45 +0800

    新增 docker compose 相关文件，可以通过 docker compose 快速部署测试

--- samples/OrderManagement/Program.cs
+++ samples/OrderManagement/Program.cs
@@ -19,12 +19,8 @@ builder.Services.AddSwaggerGen(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 
